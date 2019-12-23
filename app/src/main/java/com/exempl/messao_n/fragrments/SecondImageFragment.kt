af7c25436989ea3519c38e70d17872ebcee3824d
naim9432 <naim94600@gmail.com>
2019-12-23 16:25:56 +0100

    First fragment done

--- app/src/main/java/com/exempl/messao_n/fragrments/SecondImageFragment.kt
+++ app/src/main/java/com/exempl/messao_n/fragrments/SecondImageFragment.kt
@@ -2,10 +2,15 @@ package com.exempl.messao_n.fragrments
 
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -13,41 +18,94 @@ import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_first_image.*
 import kotlinx.android.synthetic.main.fragment_second_image.*
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SecondImageFragment : Fragment() {
 
-    private val imageUrl = "https://www.w3schools.com/w3css/img_lights.jpg"
+    class MainActivity : AppCompatActivity() {
 
-    private fun loadIamgeUsingGlide() {
-        secondFragmentprogressBar.visibility = View.VISIBLE
-        GlideApp.with(context!!).asBitmap()
-                .load(Uri.parse(imageUrl))
-                .into(object : BitmapImageViewTarget(secondFragmentImageView){
-                    override fun onResourceReady(
-                        resource: Bitmap,
-                        transition: Transition<in Bitmap>?
-                    ) {
-                        super.onResourceReady(resource, transition)
-                        secondFragmentprogressBar.visibility = View.INVISIBLE
-                        secondFragmentImageView.isVisible = true
-                    }
-                })
-    }
+        val CITY: String = "Paris,FR"
+        val API: String = "1687f993de414bc909d55ce66fe2821b"
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        loadIamgeUsingGlide()
-    }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-    companion object {
-        fun newInstance() = SecondImageFragment()
-    }
+            weatherTask().execute()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_second_image, container, false)
+        }
+
+        inner class weatherTask() : AsyncTask<String, Void, String>() {
+            override fun onPreExecute() {
+                super.onPreExecute()
+                findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+                findViewById<TextView>(R.id.errorText).visibility = View.GONE
+            }
+
+            override fun doInBackground(vararg params: String?): String? {
+                var response:String?
+                try{
+                    response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
+                        Charsets.UTF_8
+                    )
+                }catch (e: Exception){
+                    response = null
+                }
+                return response
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                try {
+                    /* Extracting JSON returns from the API */
+                    val jsonObj = JSONObject(result)
+                    val main = jsonObj.getJSONObject("main")
+                    val sys = jsonObj.getJSONObject("sys")
+                    val wind = jsonObj.getJSONObject("wind")
+                    val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                    val updatedAt:Long = jsonObj.getLong("dt")
+                    val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                    val temp = main.getString("temp")+"°C"
+                    val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                    val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                    val pressure = main.getString("pressure")
+                    val humidity = main.getString("humidity")
+
+                    val sunrise:Long = sys.getLong("sunrise")
+                    val sunset:Long = sys.getLong("sunset")
+                    val windSpeed = wind.getString("speed")
+                    val weatherDescription = weather.getString("description")
+
+                    val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                    findViewById<TextView>(R.id.address).text = address
+                    findViewById<TextView>(R.id.updated_at).text =  updatedAtText
+                    findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+                    findViewById<TextView>(R.id.temp).text = temp
+                    findViewById<TextView>(R.id.temp_min).text = tempMin
+                    findViewById<TextView>(R.id.temp_max).text = tempMax
+                    findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                    findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                    findViewById<TextView>(R.id.wind).text = windSpeed
+                    findViewById<TextView>(R.id.pressure).text = pressure
+                    findViewById<TextView>(R.id.humidity).text = humidity
+
+                    /* Views populated, Hiding the loader, Showing the main design */
+                    findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                    findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+
+                } catch (e: Exception) {
+                    findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                    findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+                }
+
+            }
+        }
     }
 }
